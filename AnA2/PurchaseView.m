04d2d6ce0bd1d71f66932f4fefc09d5cdef9210f
Brad Chruszcz <chrusb@MD105412.local>
2012-01-15 14:04:30 -0500

    Move files around, add Purchase View skeleton

---
+++ AnA2/PurchaseView.m
@@ -0,0 +1,51 @@
+//
+//  PurchaseView.m
+//  AnA2
+//
+//  Created by Chruszcz, Brad on 12-01-15.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PurchaseView.h"
+
+@implementation PurchaseView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	return YES;
+}
+
+@end
