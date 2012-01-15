34d96a34f487e949f725db4cebb0d18e3ddbf8e5
Brad Chruszcz <chrusb@MD105412.local>
2012-01-15 16:06:04 -0500

    Nav bar that switches pages, now with wrapping!

--- AnA2/ViewController.h
+++ AnA2/ViewController.h
@@ -9,17 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController {
-    UILabel *factionLabel;
-    
     UISwitch* tvButton;
     UIPageControl* pageControl;
+    
+    UIButton* navNextButton;
+    UIButton* navPrevButton;
     
     IBOutlet UIScrollView *scrollView;
 }
 
-@property (nonatomic, retain) IBOutlet UILabel *factionLabel;
-
 @property (nonatomic, retain) IBOutlet UISwitch *tvButton;
+
+@property (nonatomic, retain) IBOutlet UIButton *navNextButton;
+@property (nonatomic, retain) IBOutlet UIButton *navPrevButton;
 
 @property (nonatomic, retain) IBOutlet UIScrollView* scrollView;
 @property (nonatomic, retain) IBOutlet UIPageControl* pageControl;
@@ -28,4 +30,7 @@
 -(IBAction) changePage;
 
 -(void) tvOut;
+-(void) setPage: (int) page;
+-(int) getPage;
+
 @end
